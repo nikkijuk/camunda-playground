7ec7af4f3ba7db28ef25ed7d92102804415a06a4
jnikki <jukka.nikki@kolumbus.fi>
2019-01-28 10:06:47 +0100

    still not complete basic implementation -- jooq code generation with in memory database is maybe bad idea?

---
+++ camunda-boot-kotlin-liquibase-jooq/loanapproval/src/main/kotlin/com/jukkanikki/loanapproval/LoanDelegates.kt
@@ -0,0 +1,17 @@
+package com.jukkanikki.loanapproval
+
+import org.camunda.bpm.engine.delegate.DelegateExecution
+import org.camunda.bpm.engine.delegate.JavaDelegate
+
+/**
+ * Log all variables to console
+ */
+class LogVariables : JavaDelegate {
+
+    @Throws(Exception::class)
+    override fun execute(execution: DelegateExecution) {
+        val variables = execution.variables
+        variables.keys.iterator().forEach{ key -> println("key $key, value ${variables.get(key)}") }
+    }
+
+}
---
+++ camunda-boot-kotlin-liquibase-jooq/loanapproval/src/main/kotlin/com/jukkanikki/loanapproval/LoanRequest.kt
@@ -0,0 +1,6 @@
+package com.jukkanikki.loanapproval
+
+/**
+ * Simple container for loan request response, contains only id of started loan request process
+ */
+data class LoanRequest(val id: String? = "")
